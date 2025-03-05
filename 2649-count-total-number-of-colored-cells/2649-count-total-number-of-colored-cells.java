dc4053f8ef5d94fbca7118a357253049d1ad5c95
Zeal Shah <zealshah396@gmail.com>
2025-03-05 07:46:48 -0500

    Time: 0 ms (100%), Space: 40.7 MB (26.23%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,9 +1,10 @@
 class Solution {
     public long coloredCells(int n) {
-        long toatalNumberOfCells =0;
-        for(long i=1; i<=(2*n)-1; i++){
-            if(i%2 != 0) toatalNumberOfCells+=i;
-        }
-        return (toatalNumberOfCells*2) - ((2*n)-1);
+        // long toatalNumberOfCells =0;
+        // for(long i=1; i<=(2*n)-1; i++){
+        //     if(i%2 != 0) toatalNumberOfCells+=i;
+        // }
+        // return (toatalNumberOfCells*2) - ((2*n)-1);
+        return 1 + 4L * n *(n-1)/2 ;
     }
 }
