7ccef56f05883c2f0707eb0dbc336250ba207e11
Zeal Shah <zealshah396@gmail.com>
2025-03-04 21:52:27 -0500

    Time: 5 ms (53.61%), Space: 42.6 MB (99.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
+        if(s.equals(t)) return true;
         
         int[] arr = new int[26];
         for(int i=0; i< s.length(); i++){
