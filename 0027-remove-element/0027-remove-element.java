33cde75fd5223a85886ad342c071409300591ddc
Zeal Shah <zealshah396@gmail.com>
2025-03-07 07:30:18 -0500

    Time: 0 ms (100%), Space: 42.1 MB (46.15%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0, right = nums.length-1, count = nums.length-1;
+        while(left<=right){
+            if(nums[right] == val){
+                nums[right] = -1;
+                right--;
+            }
+
+            else if(nums[left] == val){
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                nums[right] = -1;
+                left++;
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return left;
+    }
+}
