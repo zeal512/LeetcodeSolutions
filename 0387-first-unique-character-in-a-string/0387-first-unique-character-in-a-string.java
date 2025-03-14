1b54872569e871b77752748b00428f26e315340f
Zeal Shah <zealshah396@gmail.com>
2025-03-14 16:41:19 -0400

    Time: 6 ms (82.02%), Space: 45.3 MB (65.21%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,19 +3,22 @@ class Solution {
         if(s.length() == 1) return 0;
         int arrSize = 26;
         int[] arr = new int[arrSize];
-        Arrays.fill(arr, -1);
-        int minVal = Integer.MAX_VALUE;
+        Arrays.fill(arr, 0);
+        //int minVal = Integer.MAX_VALUE;
         
         for(int i=0; i< s.length(); i++){
-            if(arr[s.charAt(i) - 'a'] == -1){
-                arr[s.charAt(i) - 'a'] = i;
-            }
-            else arr[s.charAt(i) - 'a'] = -2;
+            // if(arr[s.charAt(i) - 'a'] == -1){
+            //     arr[s.charAt(i) - 'a'] = i;
+            // }
+            // else arr[s.charAt(i) - 'a'] = -2;
+            arr[s.charAt(i) - 'a']++;
         }
 
-        for(int i=0; i< arrSize; i++){
-            if(arr[i] >=0) minVal = Math.min(minVal, arr[i]);
+        for(int i=0; i< s.length(); i++){
+            //if(arr[i] >=0) minVal = Math.min(minVal, arr[i]);
+            if(arr[s.charAt(i) - 'a'] == 1) return i;
         }
-        return (minVal == Integer.MAX_VALUE) ? -1: minVal;
+        //return (minVal == Integer.MAX_VALUE) ? -1: minVal;
+        return -1;
     }
 }
