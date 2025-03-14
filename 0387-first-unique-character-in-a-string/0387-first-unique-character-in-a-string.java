4c13645b71a3a70450da390aa1548a2c8d2106ac
Zeal Shah <zealshah396@gmail.com>
2025-03-14 16:42:02 -0400

    Time: 6 ms (82.02%), Space: 45.1 MB (81.48%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,12 +3,9 @@ class Solution {
         if(s.length() == 1) return 0;
         int arrSize = 26;
         int[] arr = new int[arrSize];
-        Arrays.fill(arr, 0);
-        
         for(int i=0; i< s.length(); i++){
             arr[s.charAt(i) - 'a']++;
         }
-
         for(int i=0; i< s.length(); i++){
             if(arr[s.charAt(i) - 'a'] == 1) return i;
         }
