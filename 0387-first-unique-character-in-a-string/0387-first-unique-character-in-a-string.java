ed2efcd252c0ea79e5aaa8fd0ac417ff8bf6bfdd
Zeal Shah <zealshah396@gmail.com>
2025-03-14 16:41:45 -0400

    Time: 7 ms (63.34%), Space: 45.6 MB (10.9%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -4,21 +4,14 @@ class Solution {
         int arrSize = 26;
         int[] arr = new int[arrSize];
         Arrays.fill(arr, 0);
-        //int minVal = Integer.MAX_VALUE;
         
         for(int i=0; i< s.length(); i++){
-            // if(arr[s.charAt(i) - 'a'] == -1){
-            //     arr[s.charAt(i) - 'a'] = i;
-            // }
-            // else arr[s.charAt(i) - 'a'] = -2;
             arr[s.charAt(i) - 'a']++;
         }
 
         for(int i=0; i< s.length(); i++){
-            //if(arr[i] >=0) minVal = Math.min(minVal, arr[i]);
             if(arr[s.charAt(i) - 'a'] == 1) return i;
         }
-        //return (minVal == Integer.MAX_VALUE) ? -1: minVal;
         return -1;
     }
 }
