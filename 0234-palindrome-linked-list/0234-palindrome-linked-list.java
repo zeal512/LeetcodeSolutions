86a3d0a3754303635866b1567139ebfbc8312fbc
Zeal Shah <zealshah396@gmail.com>
2025-03-16 10:01:00 -0400

    Time: 4 ms (82.71%), Space: 59.5 MB (72.28%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null) return true; 
