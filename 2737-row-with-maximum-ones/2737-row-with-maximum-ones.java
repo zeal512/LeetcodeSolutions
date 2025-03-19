a91b0458e5fabec03fbef63f6ffde5ed6d2ffee6
Zeal Shah <zealshah396@gmail.com>
2025-03-19 07:51:19 -0400

    Time: 4 ms (82.64%), Space: 47.2 MB (5.05%) - LeetHub

---
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        int[] result = new int[2];
+        for(int i=0; i< mat.length; i++){
+            int count =0;
+            for(int j=0; j< mat[0].length; j++){
+                if(mat[i][j] == 1) count++;
+            }
+            if(count > result[1]){
+                result[0] = i;
+                result[1] = count;
+            }
+        }
+        return result;
+    }
+}
