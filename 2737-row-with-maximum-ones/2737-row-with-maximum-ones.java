2584d51e58cfc948f3a31566c63c8e9126839c5e
Zeal Shah <zealshah396@gmail.com>
2025-03-19 07:52:19 -0400

    Time: 4 ms (82.64%), Space: 46.7 MB (55.46%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
@@ -3,7 +3,7 @@ class Solution {
         int[] result = new int[2];
         for(int i=0; i< mat.length; i++){
             int count =0;
-            for(int j=0; j< mat[0].length; j++){
+            for(int j=0; j< mat[i].length; j++){
                 if(mat[i][j] == 1) count++;
             }
             if(count > result[1]){
