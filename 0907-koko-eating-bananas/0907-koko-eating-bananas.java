b7656ba6c748549a465416d63d21cee0680369a7
Zeal Shah <zealshah396@gmail.com>
2025-03-28 23:32:04 -0400

    Time: 8 ms (84.62%), Space: 45 MB (88.19%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left =1, right = 1_000_000_000;
+        long minSpeed = right;
+        while(left <= right){
+            int speed = left + (right - left) /2;
+            long maxHours = calculateTime(speed, piles);
+            if(maxHours <= h){
+                minSpeed = speed;
+                right = speed -1;
+            }
+            else{
+                left = speed +1;
+            }
+        }
+        return (int)minSpeed;
+    }
+
+    /* public static int findMaxBananasInPile(int[] piles){
+        int max =piles[0]; 
+        for(int i=0; i< piles.length; i++){
+            max = Math.max(max, piles[i]);
+        }
+        return max;
+    } */
+    
+    public static long calculateTime(int speed, int[] piles){
+        long time =0; 
+        for (int pile : piles) {
+            time += (pile + speed - 1L) / speed;
+        }
+        return time;
+    }
+}
