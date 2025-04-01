4f42d56631d724774245b463a0485561e56f0b3d
Zeal Shah <zealshah396@gmail.com>
2025-04-01 13:43:07 -0400

    Time: 0 ms (100%), Space: 40.9 MB (6.38%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int fib(int n) {
+    /* public int fib(int n) {
         if(n <=1) return n;
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
@@ -10,7 +10,17 @@ class Solution {
         if(n <=1) return n;
         if(dp[n] != -1) return dp[n];
         return dp[n] = calculateFib(n-1, dp) + calculateFib(n-2, dp);
-    }
+    } */
 
+    //Memoization 
+    public int fib(int n){
+        int num1 = 0, num2 = 1;
+        for(int i=0; i<n; i++){
+            int sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num1;
+    }
 
 }
