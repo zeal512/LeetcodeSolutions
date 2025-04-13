c58f9c72cbe0ac0d12ae163dfed17784dab25bcc
Zeal Shah <zealshah396@gmail.com>
2025-04-13 10:46:18 -0400

    Time: 0 ms (100%), Space: 40.7 MB (80.1%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n == 0) return false;
+        return n>0 && (n & (n-1)) == 0;
+        /* if(n <= 0) return false;
         if(n == 1) return true;
-        return n%2 == 0 && isPowerOfTwo(n/2);
+        return n%2 == 0 && isPowerOfTwo(n/2); */
     }
 }
