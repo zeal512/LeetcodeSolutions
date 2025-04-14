2b644c4046394fbe7f62039b8314a4e2f9eb5a28
Zeal Shah <zealshah396@gmail.com>
2025-04-14 07:27:59 -0400

    Time: 14 ms (21.88%), Space: 41.5 MB (45.6%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.java
+++ 1656-count-good-triplets/1656-count-good-triplets.java
@@ -1,24 +1,16 @@
-class Pair{
-    int i,j,k;
-    Pair(int i, int j, int k){
-        this.i = i;
-        this.j = j;
-        this.k = k;
-    }
-}
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        List<Pair> pairs = new ArrayList<>();
+        int count =0;
         for(int i=0; i< arr.length-2; i++){
             for(int j=i+1; j< arr.length-1; j++){
                 for(int k=j+1; k< arr.length; k++){
                     if(Math.abs(arr[i] - arr[j])<= a && Math.abs(arr[j] - arr[k]) <=b &&
                        Math.abs(arr[i] - arr[k]) <=c){
-                        pairs.add(new Pair(i,j,k));
+                        count++;
                        }
                 }
             }
         }
-        return pairs.size();
+        return count;
     }
 }
