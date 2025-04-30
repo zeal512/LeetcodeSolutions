962a273fb26973d0e706143214ae1db880091956
Zeal Shah <zealshah396@gmail.com>
2025-04-30 14:50:22 -0400

    Time: 2 ms (82.9%), Space: 44.4 MB (98.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> magzineLetter = new HashMap<>();
+        //Map<Character, Integer> magzineLetter = new HashMap<>();
+        int[] letters = new int[26];
         for(char ch : magazine.toCharArray()){
-            magzineLetter.put(ch, magzineLetter.getOrDefault(ch, 0) + 1);
+            letters[ch - 'a'] +=1; 
+           // magzineLetter.put(ch, magzineLetter.getOrDefault(ch, 0) + 1);
         }
         for(char c : ransomNote.toCharArray()){
-            if (!magzineLetter.containsKey(c) || magzineLetter.get(c) <=0) return false;
-            
-            magzineLetter.put(c, magzineLetter.get(c) -1 );
+            if(letters[c - 'a'] <=0 ) return false;
+            letters[c - 'a'] -=1;
+            /* if (!magzineLetter.containsKey(c) || magzineLetter.get(c) <=0) return false;
+            magzineLetter.put(c, magzineLetter.get(c) -1 ); */
         }
         return true;
     }
