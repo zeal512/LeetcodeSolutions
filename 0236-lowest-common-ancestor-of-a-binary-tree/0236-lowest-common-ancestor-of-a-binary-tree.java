6434f3b3754682bf92bc296cd1a086b74d171e6e
Zeal Shah <zealshah396@gmail.com>
2025-06-25 08:40:17 -0400

    Time: 8 ms (18.93%), Space: 44.2 MB (99.4%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,40 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        List<TreeNode> pathOfP = new ArrayList<>();
+        traverseDFS(root, p, pathOfP);
+        
+        List<TreeNode> pathOfQ = new ArrayList<>();
+        traverseDFS(root, q, pathOfQ);
+        
+        for (int i = pathOfQ.size()-1; i >=0; i--) {
+            if (pathOfP.contains(pathOfQ.get(i))) {
+                return pathOfQ.get(i);
+            }
+        }
+
+        /* for (int i = pathOfQ.size()-1; i >=0 ; i--) {
+            if (setOfP.contains(pathOfQ.get(i))) {
+                return pathOfQ.get(i);
+            }
+        } */
+        return root;
+    }
+
+    public static boolean traverseDFS(TreeNode node, TreeNode targetNode,  List<TreeNode> path ){
+        if (node == null) {
+            return false;
+        }
+        path.add(node);
+        if (node == targetNode) {
+            return true;
+        }
+        
+        if(traverseDFS(node.left, targetNode, path) || traverseDFS(node.right, targetNode, path)) {
+            return true;
+        }
+
+        path.remove( path.size()-1 );
+        return false;
+    }
+}
