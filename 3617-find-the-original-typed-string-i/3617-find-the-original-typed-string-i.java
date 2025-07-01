f4f9daf1e7b58e8a3c8824940cc9d1e9855dee7d
Zeal Shah <zealshah396@gmail.com>
2025-07-01 09:39:22 -0400

    Time: 0 ms (100%), Space: 42.2 MB (61.16%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,6 +1,5 @@
 class Solution {
     public int possibleStringCount(String word) {
-        //Map<Character, Integer> countChar = new HashMap<>();
         int possibilities = 1;
         for (int i = 1; i < word.length(); i++) {
             if (word.charAt(i) == word.charAt(i-1) ) {
@@ -8,14 +7,5 @@ class Solution {
             }
         }
         return possibilities;
-        /* for(char ch: word.toCharArray()){
-            countChar.put(ch, countChar.getOrDefault(ch, 0) + 1);
-        }
-        for(int value : countChar.values()){
-            if(value > 1){
-                possibilities += (value - 1);
-            }
-        }
-        return possibilities; */
     }
 }
