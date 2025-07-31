c41a42225044af817b5dd65d76588a123ec2ca88
Zeal Shah <zealshah396@gmail.com>
2025-07-31 01:29:11 -0400

    Time: 2 ms (12.25%), Space: 41.9 MB (33.14%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int hills =0, vally =0;
+        int hillsOrVally =0;
         List<Integer> unique = new ArrayList<>();
         unique.add(nums[0]);
         for (int i = 1; i < nums.length; i++) {
@@ -9,13 +9,11 @@ class Solution {
             }
         }
         for (int i = 1; i < unique.size()-1; i++) {
-            if (unique.get(i) > unique.get(i-1) &&  unique.get(i) > unique.get(i+1)) {
-                hills++;
-            }
-            else if(unique.get(i) < unique.get(i-1) &&  unique.get(i) < unique.get(i+1)){
-                vally++;
+            if ((unique.get(i) > unique.get(i-1) &&  unique.get(i) > unique.get(i+1)) || 
+                (unique.get(i) < unique.get(i-1) &&  unique.get(i) < unique.get(i+1) ) ) {
+                hillsOrVally++;
             }
         }
-        return hills + vally;
+        return hillsOrVally;
     }
 }
