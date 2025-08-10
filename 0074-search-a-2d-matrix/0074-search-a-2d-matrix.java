9436e73d88bde38beca721421ac68d3edc39c01d
Zeal Shah <zealshah396@gmail.com>
2025-08-10 11:50:19 -0400

    Time: 0 ms (100%), Space: 42 MB (81.61%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
+        /* for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 int len = matrix[0].length-1;
                 if (target == matrix[i][j]) return true;
@@ -8,6 +8,16 @@ class Solution {
                     break;
                 }
             }
+        }
+        return false; */
+        int m =matrix.length, n = matrix[0].length;
+        int left =0, right = (m*n)-1;
+        while (left <= right) {
+            int mid = (left + right)/2;
+            int i = mid/n, j=mid%n;
+            if (matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) right = mid-1;
+            else left = mid+1;
         }
         return false;
     }
