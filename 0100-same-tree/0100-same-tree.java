c85412ee5461ee0e77db6447f6c6811ba397aaf3
Zeal Shah <zealshah396@gmail.com>
2025-08-18 16:46:24 -0400

    Time: 0 ms (100%), Space: 41.3 MB (29.74%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -21,10 +21,7 @@ class Solution {
         return checkBalanced(p.left, q.left) && checkBalanced(p.right, q.right);
     } */
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if( p == null && q == null) return true;
-        if((p == null & q!= null) || (p!= null && q == null)) return false;
-        if(p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        //return checkBalanced(p,q);
+        if( p == null || q == null) return p==q;
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
