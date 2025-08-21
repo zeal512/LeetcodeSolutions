e2faae0239e5dcfdb1ad806f0aaa258c70e32b03
Zeal Shah <zealshah396@gmail.com>
2025-08-20 22:01:43 -0400

    Time: 3 ms (40.59%), Space: 42.4 MB (6.86%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,6 +1,6 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        if (s.length() == 0) return t.charAt(0);
+        
         int bit = 0;
         for (Character ch : s.toCharArray()) {
             bit = bit ^ ch;
