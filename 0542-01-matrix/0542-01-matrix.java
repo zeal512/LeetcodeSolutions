74773ef258968358876e3fe16fbeb01f3b4edefa
Zeal Shah <zealshah396@gmail.com>
2025-10-07 12:55:27 -0400

    Time: 14 ms (56.19%), Space: 46.8 MB (90.86%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,4 +1,4 @@
-class Pair{
+/* class Pair{
     int row;
     int col;
     int distance;
@@ -7,18 +7,20 @@ class Pair{
         this.col = col;
         this.distance = distance;
     }
-}
+} */
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int[][] visited = new int[m][n];
         int[][] result = new int[m][n];
-        Queue<Pair> queue = new LinkedList<>();
+        //Queue<Pair> queue = new LinkedList<>();
+        Queue<int[]> queue= new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j =0; j<n;j++){
                 if(mat[i][j]==0){
-                    queue.add(new Pair(i,j,0));
+                    //queue.add(new Pair(i,j,0));
+                    queue.add(new int[]{i,j,0});
                     visited[i][j] =1;
                     result[i][j] = 0;
                 }
@@ -29,22 +31,23 @@ class Solution {
         int[] dCol = {0,-1,0,1};
 
         while(!queue.isEmpty()){
-            int r = queue.peek().row;
+            /* int r = queue.peek().row;
             int c = queue.peek().col;
-            int dist = queue.peek().distance;
-            queue.remove();
+            int dist = queue.peek().distance; */
+            int[] curr = queue.poll();
+            int r = curr[0], c = curr[1], dist = curr[2];
+            //queue.remove();
             result[r][c] = dist;
             for(int i=0;i<dRow.length;i++){
                 int nRow = r + dRow[i];
                 int nCol = c + dCol[i];
                 if(nRow >=0 && nRow<m && nCol >=0 && nCol < n && visited[nRow][nCol] == 0){
                     visited[nRow][nCol] = 1;
-                    queue.add(new Pair(nRow, nCol, dist+1));
+                    /* queue.add(new Pair(nRow, nCol, dist+1)); */
+                    queue.add(new int[]{nRow, nCol, dist+1});
                    }
             }
         }
-
         return result;
-
     }
 }
