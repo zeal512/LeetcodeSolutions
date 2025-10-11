9b4aaa7e6eff0a3b8eb40e2d877f089b6e9b1a7d
Zeal Shah <zealshah396@gmail.com>
2025-10-11 15:49:10 -0400

    Time: 2 ms (98.48%), Space: 42.5 MB (8.54%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
+        /* Map<Integer, Integer> map = new HashMap<>();
         for (int num : arr) {
             map.put(num, map.getOrDefault(num,0) +1 );
         }
@@ -9,6 +9,19 @@ class Solution {
             if (set.contains(num)) return false;
             set.add(num);
         }
+        return true; */
+        int helper = 1000;
+        int[] counter = new int[2001];
+        for (int i = 0; i < arr.length; i++) {
+            counter[arr[i] + helper]++;
+        }
+        boolean[] encountered = new boolean[2001];
+        for (int i = 0; i < counter.length; i++) {
+            if(counter[i] == 0) continue;
+            if(encountered[counter[i]]) return false;
+            encountered[counter[i]] = true;
+        }
         return true;
     }
+
 }
