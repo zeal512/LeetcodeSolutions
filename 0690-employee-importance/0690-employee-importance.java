8003cb71fbe0603baaf6ea88b6e2cf9d39f2fcbe
Zeal Shah <zealshah396@gmail.com>
2025-10-14 10:52:44 -0400

    Time: 5 ms (51.2%), Space: 46.3 MB (76.08%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.java
+++ 0690-employee-importance/0690-employee-importance.java
@@ -10,22 +10,30 @@ class Employee {
 class Solution {
 
     public int getImportance(List<Employee> employees, int id) {
-        Map<Integer, Employee> idMapping = new HashMap<>();
+        // Map<Integer, Employee> idMapping = new HashMap<>();
+        Employee[] idMappingArray = new Employee[2001];
         for (Employee employee : employees) {
-            idMapping.put(employee.id, employee);
+            //idMapping.put(employee.id, employee);
+            idMappingArray[employee.id] = employee;
         }
 
         Queue<List<Integer>> queue = new LinkedList<>();
         int total_importance = 0;
-        total_importance += idMapping.get(id).importance;
-        queue.offer(idMapping.get(id).subordinates);
+        /* total_importance += idMapping.get(id).importance;
+        queue.offer(idMapping.get(id).subordinates); */
+        total_importance += idMappingArray[id].importance;
+        queue.offer(idMappingArray[id].subordinates);
 
         while(!queue.isEmpty()){
             List<Integer> sub = queue.poll();
             for (Integer _subordinate : sub) {
-                total_importance += idMapping.get(_subordinate).importance;
+                /* total_importance += idMapping.get(_subordinate).importance;
                 if (idMapping.get(_subordinate) != null) {
                     queue.offer(idMapping.get(_subordinate).subordinates );
+                } */
+                total_importance += idMappingArray[_subordinate].importance;
+                if(idMappingArray[_subordinate] != null){
+                    queue.offer(idMappingArray[_subordinate].subordinates);
                 }
             }
         }
