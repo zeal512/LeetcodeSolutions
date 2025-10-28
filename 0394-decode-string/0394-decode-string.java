960583f31d870b87f3b30bf20b6599da66d49ced
Zeal Shah <zealshah396@gmail.com>
2025-10-27 20:47:06 -0400

    Time: 2 ms (27.79%), Space: 42.2 MB (8.4%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -8,26 +8,25 @@ class Solution {
             else{
                 StringBuilder builder = new StringBuilder();
                 while (!stack.isEmpty() && stack.peek() != '[') {
-                    builder.append(stack.pop());
+                    builder.insert(0, stack.pop());
                 }
-                builder.reverse();
+                
                 stack.pop();
                 StringBuilder num = new StringBuilder();
                 while (!stack.isEmpty() && Character.isDigit(stack.peek()) ) {
-                    num.append(stack.pop());
+                    num.insert(0, stack.pop());
                 }
-                num.reverse();
                 String newString = builder.toString().repeat(Integer.parseInt(num.toString()) );
+                
                 for (Character character : newString.toCharArray()) {
                     stack.push(character);
                 }
-                /* stack.push(builder.toString().repeat(Integer.parseInt(num.toString()) )); */
             }
         }
         StringBuilder sb = new StringBuilder();
         while (!stack.isEmpty()) {
-            sb.append(stack.pop());
+            sb.insert(0, stack.pop());
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
