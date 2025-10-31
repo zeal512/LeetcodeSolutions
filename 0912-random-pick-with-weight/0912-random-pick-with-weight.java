bb31a9c912ff7dc1ed869292c8a08584d913a3df
Zeal Shah <zealshah396@gmail.com>
2025-10-31 10:10:19 -0400

    Time: 30 ms (19.71%), Space: 53 MB (6.07%) - LeetHub

---
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -0,0 +1,33 @@
+class Solution {
+    List<Integer> preSum = new ArrayList<>();
+    public Solution(int[] w) {
+        int[] prefixSum = new int[w.length];
+        int sum = 0;
+        for(int i=0; i<w.length; i++){
+            sum+=w[i];
+            prefixSum[i] = sum;
+            preSum.add(sum);
+        }
+    }
+    
+    public int pickIndex() {
+        Random random = new Random();
+        int range = preSum.get(preSum.size()-1);
+        int randomNum = random.nextInt(range) + 1;
+        
+        int low = 0, high = preSum.size()-1;
+        while(low<= high){
+            int mid = (low+high)/2;
+            if(preSum.get(mid) == randomNum) return mid;
+            else if(preSum.get(mid) >= randomNum) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
