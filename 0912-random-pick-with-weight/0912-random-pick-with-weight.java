33204a17bbc6ca63cc2624885b57d1a22084a0ce
Zeal Shah <zealshah396@gmail.com>
2025-10-31 10:18:43 -0400

    Time: 24 ms (75.15%), Space: 51.7 MB (6.07%) - LeetHub

--- 0912-random-pick-with-weight/0912-random-pick-with-weight.java
+++ 0912-random-pick-with-weight/0912-random-pick-with-weight.java
@@ -1,23 +1,24 @@
 class Solution {
-    List<Integer> preSum = new ArrayList<>();
+    private final Random random = new Random();;
+    private final int[] prefixSum;
+    private final int total;
     public Solution(int[] w) {
+        prefixSum = new int[w.length];
         int sum = 0;
         for(int i=0; i<w.length; i++){
             sum+=w[i];
-            preSum.add(sum);
+            prefixSum[i] = sum;
         }
+        total = sum;
     }
     
     public int pickIndex() {
-        Random random = new Random();
-        int range = preSum.get(preSum.size()-1);
-        int randomNum = random.nextInt(range) + 1;
-        
-        int low = 0, high = preSum.size()-1;
+        int randomNum = random.nextInt(total) + 1;
+        int low = 0, high = prefixSum.length-1;
         while(low<= high){
             int mid = (low+high)/2;
-            if(preSum.get(mid) == randomNum) return mid;
-            else if(preSum.get(mid) >= randomNum) high = mid-1;
+            if(prefixSum[mid] == randomNum) return mid;
+            else if(prefixSum[mid] >= randomNum) high = mid-1;
             else low = mid+1;
         }
         return low;
