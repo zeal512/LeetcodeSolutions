1eb36c4f3efe658c85b88a4a731c2847f4f4976b
Zeal Shah <zealshah396@gmail.com>
2025-11-26 21:05:49 -0500

    Time: 44 ms (0.71%), Space: 42 MB (7.81%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -3,23 +3,9 @@ class Solution {
         int total = 0;
         total+=numBottles;
         while(numBottles >= numExchange){
-            System.out.println(total);
             total += numBottles/numExchange;
-            
             numBottles = (numBottles/numExchange) + (numBottles % numExchange);
         }
         return total;
     }
 }
-/* 
-numBottles = 9 , (9/3) + (9%3) = 3, 1
-numExchange = 3
-total = 9 , (9+3) = 12, 13
-
-
-
-
-
-
-
- */
