b98ff8dc811f19675ef95d547a019dbf42cc2f54
Zeal Shah <zealshah396@gmail.com>
2025-11-30 12:05:19 -0500

    Time: 1 ms (99.64%), Space: 77.3 MB (11.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,14 +3,10 @@ class Solution {
         int sum = 0, maxSum = Integer.MIN_VALUE;
         for(int i=0; i< nums.length; i++){
             sum +=nums[i];
-            if(sum > maxSum){
-                maxSum = sum;
-            }
-            if(sum < 0){
-                sum =0;
-            }
+            maxSum = Math.max(maxSum, sum);
+            if(sum < 0)sum =0;
         }
-        if(maxSum < 0) return maxSum;
+        //if(maxSum < 0) return maxSum;
         return maxSum;
     }
 }
