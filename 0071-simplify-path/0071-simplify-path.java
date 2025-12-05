d780890a426f755dad8cdf9bc07c4109a2ca289e
Zeal Shah <zealshah396@gmail.com>
2025-12-05 14:57:18 -0500

    Time: 4 ms (94.28%), Space: 44.7 MB (50.73%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -16,9 +16,43 @@ class Solution {
         }
         if(stack.isEmpty()) return "/";
         
-        for (String str: stack) {
+        for (String str : stack) {
             result.append("/").append(str);
         }
         return result.toString();
+        
+        /* int i = 0;
+
+        while(i < path.length()) {
+            while (i < path.length() && path.charAt(i) == '.') {
+                builder.append(path.charAt(i));
+                i+=1;
+            }
+            if(builder.length() > 0 && builder.toString().equals("..") && !stack.isEmpty() ){
+                stack.pop();
+            }else if(builder.length() > 0){
+                stack.push(builder.toString());
+            }
+
+            builder.delete(0, builder.toString().length());
+            
+            
+            while(path.charAt(i) != '/' && i < path.length() ){
+                builder.append(path.charAt(i));
+                i+=1;
+            }
+            if(builder.length() > 0){
+                stack.push(builder.toString());
+                builder.delete(0, builder.toString().length());
+            }
+        }
+        System.out.println(stack);
+        
+        while(!stack.isEmpty()){
+            result.append("/");
+            result.append(stack.pop());
+        }
+        result.delete(result.toString().length()-1, result.toString().length()); 
+        */
     }
 }
