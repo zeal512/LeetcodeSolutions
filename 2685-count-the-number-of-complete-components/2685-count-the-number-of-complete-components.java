7a2c0d0f967423b2777aa731411d295a3e9c1bbc
Zeal Shah <zealshah396@gmail.com>
2025-12-20 14:26:20 -0500

    Time: 17 ms (28.51%), Space: 48 MB (13.09%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -0,0 +1,38 @@
+class Solution {
+    public static int totalNodes = 0, totalEdges = 0;
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i =0; i< n; i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int[] edge: edges){
+            int node1 = edge[0];
+            int node2 = edge[1];
+            adjList.get(node1).add(node2);
+            adjList.get(node2).add(node1);
+        }
+        int completeConnectedComponents = 0;
+        Set<Integer> visited = new HashSet<>();
+        for(int i=0; i< n; i++){
+            if(!visited.contains(i)){
+                totalEdges = 0;
+                totalNodes = 0;
+                dfs(i, adjList, visited);
+                if(totalEdges == (totalNodes * (totalNodes -1))){
+                    completeConnectedComponents+=1;
+                }
+            }
+        }
+        return completeConnectedComponents;
+    }
+    public static void dfs(int node, List<List<Integer>> adjList, Set<Integer> visited){
+        visited.add(node);
+        totalNodes++;
+        totalEdges += adjList.get(node).size();
+        for(int neighbour: adjList.get(node)){
+            if(!visited.contains(neighbour)){
+                dfs(neighbour, adjList, visited);
+            }
+        }
+    }
+}
