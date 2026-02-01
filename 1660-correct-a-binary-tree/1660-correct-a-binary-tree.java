10e93a55dbb60ed53237264dddbc82ae0eb1e7cf
Zeal Shah <zealshah396@gmail.com>
2026-02-01 07:39:16 -0500

    Time: 13 ms (7.59%), Space: 49.4 MB (27.85%) - LeetHub

--- 1660-correct-a-binary-tree/1660-correct-a-binary-tree.java
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.java
@@ -13,48 +13,7 @@
  *     }
  * }
  */
-/*  class Pair{
-    TreeNode node, parent;
-    Pair(TreeNode node, TreeNode parent){
-        this.node = node;
-        this.parent = parent;
-    }
- }; */
 class Solution {
-    /* public TreeNode correctBinaryTree(TreeNode root) {
-        
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(root, null));
-        
-        
-        
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            Set<Integer> visited = new HashSet<>();
-            
-            for(int i=0; i< size; i++){
-                Pair cur = queue.poll();
-                TreeNode currNode = cur.node, parent = cur.parent;
-
-                if(currNode.right != null && visited.contains(currNode.right.val)){
-                    if(parent.left == currNode){
-                        parent.left = null;
-                    }else parent.right = null;
-                    return root;
-                }
-                visited.add(currNode.val);
-                if(currNode.right != null){
-                    queue.add(new Pair(currNode.right, currNode));
-                }
-                if(currNode.left != null){
-                    queue.add(new Pair(currNode.left, currNode));
-                }
-            }
-            visited.clear();
-        }
-        return root;
-    } */
-
 
     public TreeNode correctBinaryTree(TreeNode root) {
         
