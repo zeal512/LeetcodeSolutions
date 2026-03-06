27513c1e34cab45db0e5bd55fe11af3c64630a21
Zeal Shah <zealshah396@gmail.com>
2026-03-05 23:32:11 -0500

    Time: 8 ms (7.16%), Space: 46.9 MB (9.97%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,8 +1,7 @@
 class Solution {
     public String intToRoman(int num) {
-
-        int[] integerVal = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] roman = new String[]{"M", "CM", "D", "CD", "C", "XC","L", "XL", "X", "IX", "V", "IV", "I"};
+        /* int[] integerVal = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] roman = new String[]   {"M", "CM", "D", "CD", "C", "XC","L", "XL", "X", "IX", "V", "IV", "I"};
         StringBuffer result = new StringBuffer();
         for(int i=0; i< integerVal.length && num > 0; i++){
             while (num >= integerVal[i]) {
@@ -10,7 +9,31 @@ class Solution {
                 num-=integerVal[i];
             }
         }
-        return result.toString();
+        return result.toString(); */
+        Map<Integer, String> dict = new LinkedHashMap<>();
+        dict.put(1000,"M");
+        dict.put(900,"CM");
+        dict.put(500,"D");
+        dict.put(400,"CD");
+        dict.put(100,"C");
+        dict.put(90,"XC");
+        dict.put(50,"L");
+        dict.put(40,"XL");
+        dict.put(10,"X");
+        dict.put(9,"IX");
+        dict.put(5,"V");
+        dict.put(4,"IV");
+        dict.put(1,"I");
+        int temp = num;
+        StringBuffer buffer= new StringBuffer();
+        for(int val : dict.keySet()){
+            if(temp >= val){
+                int q = temp / val;
+                buffer.append(dict.get(val).repeat(q));
+                temp -=(q * val);
+            }
+        }
+        return buffer.toString();
     }
 }
 
