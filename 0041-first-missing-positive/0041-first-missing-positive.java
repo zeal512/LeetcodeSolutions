711d6c0c3df5de7f62183ef0c3291de66545fdf1
Zeal Shah <zealshah396@gmail.com>
2026-03-20 06:44:45 -0400

    Time: 2 ms (37.58%), Space: 71.1 MB (82.26%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,6 +1,6 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;        
+        /* int n = nums.length;        
         boolean[] track = new boolean[n+1];
         Arrays.fill(track, false);
         for(int i = 0; i < n; i++){
@@ -11,7 +11,31 @@ class Solution {
         for(int i =1; i <= n; i++){
             if(!track[i]) return i;
         }
-        return n+1;
+        return n+1; */
+
+        int n = nums.length;
+        boolean is1Present = false;
+        /* check if 1 is present in array and convert all the negative nums, zero and num greater than n to 1 */
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 1) is1Present = true;
+            if(nums[i] <=0 || nums[i] > n){
+                nums[i] = 1;
+            }
+        }
+        if (!is1Present) return 1;
+        for(int i =0; i< n; i++){
+            int val = Math.abs(nums[i]);
+            if(val == n){
+                nums[0] = -Math.abs(nums[0]);
+            }
+            else nums[val] = - Math.abs(nums[val]);
+        }
+        for(int i = 1; i< n; i++){
+            if(nums[i] > 0) return i;
+        }
+        return nums[0] > 0? n : n+1;
+        
+
     }
 }
 
