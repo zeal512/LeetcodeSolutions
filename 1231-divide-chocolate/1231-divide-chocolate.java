960abd6463c05275bfec51d9e7d637b4c7017420
Zeal Shah <zealshah396@gmail.com>
2026-04-02 15:35:39 -0400

    Time: 4 ms (85.08%), Space: 47 MB (70.17%) - LeetHub

---
+++ 1231-divide-chocolate/1231-divide-chocolate.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maximizeSweetness(int[] sweetness, int k) {
+        int low = 1, high = 0;
+        for(int i =0; i< sweetness.length; i++){
+            high += sweetness[i];
+        }
+
+        while(low <= high){
+            int avg = low + ((high - low)/2);
+            
+            if(totalPiecesPossible(sweetness, avg, k)){
+                low = avg +1;
+            }
+            else{
+                high = avg-1;
+            }
+        }
+        return high;
+    }
+    public boolean totalPiecesPossible(int[] sweetness, int maxSweetness, int k){
+        
+        int totalPieces = 0, sweetSum = 0;
+        for(int i =0; i < sweetness.length; i++){
+            sweetSum += sweetness[i];
+            if(sweetSum >= maxSweetness){
+                totalPieces +=1;
+                sweetSum = 0;
+            }
+        }
+        return totalPieces >=k+1;
+    }
+}
+
+/* 
+if 0 friends, sum(sweetness) will be answer
+if sweetness.length == k+1, then min(sweetness) will be answer
+so, to find out size of piece, out range should be between min(sweetness) to high = sum(sweetness)
+
+*/
