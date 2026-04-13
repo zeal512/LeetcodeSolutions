0fcfe1c1c27a718045a5c9e52274c8dda221ec5b
Zeal Shah <zealshah396@gmail.com>
2026-04-13 07:58:19 -0400

    Time: 27 ms (73.38%), Space: 56.6 MB (31.08%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        /* PriorityQueue<Integer> queue = new PriorityQueue<>();
         int[][] closest = new int[k][2];
         Map<Integer, List<int[]>> map = new HashMap<>();
         for(int[] point : points){
@@ -17,6 +17,28 @@ class Solution {
                 closest[count][1] = point[1];
                 count++;
             }
+        }
+        return closest; */
+
+        /* 
+        In the above solution, instead of using a HashMap to store the distance as key an list as pair
+        of points, we can simply store [<distance>, [index of points array]] and access the point from 
+        points[].
+        */
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        int[][] closest = new int[k][2];
+        for(int i =0; i< points.length; i++){
+            int distance = (points[i][0] * points[i][0]) + (points[i][1] * points[i][1]);
+            if(queue.size() < k){
+                queue.add(new int[]{distance, i});
+            }else if(queue.peek()[0] > distance ){
+                queue.poll();
+                queue.offer(new int[]{distance, i});
+            }
+        }
+        for(int i =0; i< k; i++){
+            int idx = queue.poll()[1];
+            closest[i] = points[idx];
         }
         return closest;
     }
