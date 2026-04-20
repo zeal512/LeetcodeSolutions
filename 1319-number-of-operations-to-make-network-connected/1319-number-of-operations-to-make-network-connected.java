d2d472713d968ff9d068c3d955c590ebf5022f6e
Zeal Shah <zealshah396@gmail.com>
2026-04-20 07:39:51 -0400

    Time: 29 ms (5.03%), Space: 77.5 MB (5.03%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -4,9 +4,7 @@ class Solution {
         if(n-1 > connections.length) return -1;
         
         Map<Integer, ArrayList<Integer>> adjList = new HashMap<>();
-        
         int total_clusters = 0;
-        
         boolean[] visited = new boolean[n];
         
         for(int[] connection : connections){
@@ -16,14 +14,12 @@ class Solution {
             adjList.get(connection[0]).add(connection[1]);
             adjList.get(connection[1]).add(connection[0]);
         }
+        
         for(int i =0; i< n; i++){
             if(!adjList.containsKey(i)){
                 total_clusters+=1;
                 visited[i] = true;
             }
-        }
-        
-        for(int i =0; i< n; i++){
             if(!visited[i]){
                 traverse(i, adjList, visited);
                 total_clusters+=1;
