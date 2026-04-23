6f7f0d03aa6bbec0114ea3d2d596753f3a475ccb
Zeal Shah <zealshah396@gmail.com>
2026-04-23 07:18:22 -0400

    Time: 3 ms (56.92%), Space: 43.4 MB (53.81%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        int aCounter = 1;
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        StringBuffer answer = new StringBuffer();
+        String[] words = sentence.split(" ");
+        for(String word : words){
+            if(vowels.contains( Character.toLowerCase(word.charAt(0)) )){
+                answer.append(word);
+            }
+            else{
+                char first = word.charAt(0);
+                answer.append(word.substring(1));
+                answer.append(first);
+            }
+            answer.append("ma");
+            answer.append("a".repeat(aCounter++));
+            answer.append(" ");
+        }
+        return answer.substring(0, answer.toString().length()-1);
+    }
+}
