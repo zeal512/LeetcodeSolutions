0ba91506a2f237f464f55e9b824ae96eb143bd7c
Zeal Shah <zealshah396@gmail.com>
2026-04-29 08:54:32 -0400

    Time: 29 ms (83.61%), Space: 47.8 MB (88.2%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> stack = new Stack<>();
+/*         Stack<Character> stack = new Stack<>();
         for(char ch: s.toCharArray()){
             if(ch == '*'){
                 if(!stack.isEmpty()) stack.pop();
@@ -11,6 +11,12 @@ class Solution {
             sb.append(stack.pop());
         }
 
-        return sb.length() == 0 ? "" : sb.reverse().toString();
+        return sb.length() == 0 ? "" : sb.reverse().toString(); */
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch == '*') sb.deleteCharAt(sb.length()-1);
+            else sb.append(ch);
+        }
+        return sb.toString();
     }
 }
