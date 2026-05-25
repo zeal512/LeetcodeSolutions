88f9639e93d14f35fb2674cb5fb79108b07463f5
Zeal Shah <zealshah396@gmail.com>
2026-05-25 07:30:03 -0400

    Time: 47 ms (96.49%), Space: 141.8 MB (80.65%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public long kthLargestLevelSum(TreeNode root, int k) {
+    /* public long kthLargestLevelSum(TreeNode root, int k) {
         Queue<TreeNode> queue = new LinkedList<>();
         PriorityQueue<Long> pqueue = new PriorityQueue<>((a,b)-> Long.compare(b, a));
         queue.offer(root);
@@ -34,6 +34,25 @@ class Solution {
             pqueue.poll();
             k-=1;
         }
+        return pqueue.peek();
+    } */
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        PriorityQueue<Long> pqueue = new PriorityQueue<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            long sum = 0;
+            int size = queue.size();
+            for(int i =0; i < size; i++){
+                TreeNode node = queue.poll();
+                sum+=node.val;
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            pqueue.offer(sum);
+            if(pqueue.size()> k) pqueue.remove();
+        }
+        if(pqueue.size() < k) return -1;
         return pqueue.peek();
     }
 }
