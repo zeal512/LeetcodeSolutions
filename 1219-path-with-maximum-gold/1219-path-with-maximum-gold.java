66ea474c91e66fcf0b1e8cbc2a60c140ed3e0c27
Zeal Shah <zealshah396@gmail.com>
2026-05-29 08:10:44 -0400

    Time: 155 ms (5.25%), Space: 46.5 MB (11.13%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int getMaximumGold(int[][] grid) {
+        int rows = grid.length, cols = grid[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        int maxGold = 0;
+        for(int i = 0; i < rows; i ++){
+            for(int j =0; j < cols; j++){
+                if(!visited[i][j] && grid[i][j]!= 0){                    
+                    maxGold = Math.max(maxGold, dfs(i, j, visited, grid));
+                }
+            }
+        }
+        return maxGold;
+    }
+    public int dfs(int r, int c, boolean[][] visited, int[][] grid){
+        if(
+            r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || 
+            grid[r][c] == 0 || visited[r][c] 
+            ){
+                return 0;
+            }
+        visited[r][c] = true;
+        int gold = grid[r][c];
+        
+        int[][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}};
+        for(int[] dir : directions){
+            int nr = r + dir[0];
+            int nc = c + dir[1];
+            gold = Math.max(gold, grid[r][c] + dfs(nr, nc, visited, grid));
+        }
+        visited[r][c] = false;
+        return gold;
+    }
+}
